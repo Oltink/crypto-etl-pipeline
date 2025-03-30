3c30e10440d887d4d0b3f83bd3d5c5c948077ca2
Mykyta <nikitashestopalov45@gmail.com>
2025-03-30 07:21:14 -0400

    Cleaned project, added DAG and configs

---
+++ dags/crypto_pipeline.py
@@ -0,0 +1,87 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+import requests
+import psycopg2
+
+DB_CONFIG = {
+    "dbname": "crypto_db",
+    "user": "user",
+    "password": "password",
+    "host": "postgres_db",
+    "port": 5432
+}
+
+def fetch_crypto_prices():
+    url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=usd"
+    response = requests.get(url)
+    print("API Response:", response.text) 
+    return response.json()
+
+def save_to_postgres(**kwargs):
+    data = kwargs["ti"].xcom_pull(task_ids="fetch_prices")
+    if not data:
+        raise ValueError("XCom data is missing")
+
+    print("Data received from XCom:", data) 
+
+    conn = psycopg2.connect(
+    dbname=DB_CONFIG["dbname"],
+    user=DB_CONFIG["user"],
+    password=DB_CONFIG["password"],
+    host=DB_CONFIG["host"],
+    port=DB_CONFIG["port"]
+	)
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS crypto_prices (
+            id SERIAL PRIMARY KEY,
+            name TEXT,
+            price_usd FLOAT,
+            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+    """)
+
+    for coin, values in data.items():
+        cursor.execute(
+            "INSERT INTO crypto_prices (name, price_usd) VALUES (%s, %s)",
+            (coin, values["usd"])
+        )
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print("Data successfully saved to PostgreSQL") 
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2024, 2, 1),
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+dag = DAG(
+    "crypto_price_pipeline",
+    default_args=default_args,
+    schedule_interval=timedelta(hours=1),
+    catchup=False
+)
+
+fetch_task = PythonOperator(
+    task_id="fetch_prices",
+    python_callable=fetch_crypto_prices,
+    do_xcom_push=True,
+    dag=dag
+)
+
+save_task = PythonOperator(
+    task_id="save_prices",
+    python_callable=save_to_postgres,
+    dag=dag
+)
+
+fetch_task >> save_task
+
